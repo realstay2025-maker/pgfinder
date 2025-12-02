b79b4c83f3237e82d6ca4a28b44f025ebc45435f
realstay2025 <real.stay2025@gmail.com>
2025-12-03 00:22:02 +0530

    nn

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'esbuild',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     rollupOptions: {
       output: {
         manualChunks: {
